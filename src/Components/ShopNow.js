c9a675b24f7445df40cd1767f33871e3fdb75b97
sammykuria <kungukuria5724@gmail.com>
2025-09-03 10:06:07 +0300

    working on the shopnow component

--- src/Components/ShopNow.js
+++ src/Components/ShopNow.js
@@ -1,9 +1,13 @@
 import { useState,useEffect } from 'react'
+import { Link } from 'react-router-dom'
+
 
 function ShopNow() {
 
     const [products, setProducts] = useState([])
      const[searchTerm, setSearchTerm] =useState('')
+    const [liked, setLiked] = useState(false)
+
 
     useEffect(() => {
         fetch('https://fakestoreapi.com/products')
@@ -21,10 +25,29 @@ function ShopNow() {
 
 
 
+
   return (
     <div>
+        <div  className='moody'>
+            <div className='firstshooter'>
+                <img src='https://img.freepik.com/premium-vector/flash-sale-banner-social-media-sale-banner-promotion-template-discount-tag-limited-time-offer_1135545-529.jpg' alt='repeat' ></img>
+            </div>
+
+            <div className='firstshooter'>
+                <img src='https://static.independent.co.uk/2021/08/27/07/05175500-40a2bdf8-8796-4430-8bd8-e11fea6342ea.jpg' alt='repeat' ></img>
+            </div>
+
+{/* <div className="container">uooo</div> */}
+
+      
+        <div className='shopnowimage'>
+            <img className='yoimage' src='https://img.freepik.com/premium-vector/banner-template-offering-mega-sale-discount_419341-580.jpg' alt='yoo'></img>
+        </div>
+        </div>
         
         <div className='homesearch'> 
+
+            <h2>Search your product</h2>
         <div className="input-group">
             <i className="fa-solid fa-magnifying-glass"></i>
             <input placeholder="Enter text here"
@@ -37,10 +60,52 @@ function ShopNow() {
         </div>
 
 
+        <div  className='godzilla'>
+
+        <div className='shopnowcategory'>
+            <h2>Categories</h2>
+            <ul>
+                <li>
+                    <Link>Mens wear</Link>
+                </li>
+                 <li>
+                    <Link>Womens wear</Link>
+                </li>
+                <li>
+                    <Link>Childrens wear</Link>
+                </li>
+            </ul>
+            <h2>Clothing Style</h2>
+            <ul>
+             <li>
+                    <Link>Opium</Link>
+                </li>
+             <li>
+                    <Link>HipHop</Link>
+                </li>
+              <li>
+                    <Link>Old Money</Link>
+                </li>
+            <li>
+                    <Link>Old School</Link>
+                </li>
+             <li>
+                    <Link>Official Suits</Link>
+                </li>
+            </ul>
+        </div>
+
+
         <div className='gyrate'>
             {
                 filteredproducts.map(product=>(
                     <div key={product.id} className='card'>
+                        <div className='heart'>
+                            <i
+                            className={`fa-heart ${liked ? "fa-solid liked" : "fa-regular"}`}
+                            onClick={() => setLiked(!liked)}
+                            ></i>
+                        </div>
                         <div className='image-box'>
                         <img className='productimg' src={product.image} alt='yoo'></img>
                          </div>
@@ -61,7 +126,9 @@ function ShopNow() {
             }
 
         </div>
-        
+
+
+        </div>
       
     </div>
   )
